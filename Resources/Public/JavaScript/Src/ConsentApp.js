1f1b1fcd9d621d7465ec1a60519c83f1c2f9d0d5
Daniel Haupt <dh@schiffel.it>
2020-12-12 22:39:14 +0100

    [TASK] Remove obsolete functionality in ConsentApp
    As several issues have been solved in klaro, multiple workarounds are no
    longer necessary. A long-term goal is to fully drop the `ConsentApp` as
    more and more functionality is provided by klaro itself.

--- Resources/Public/JavaScript/Src/ConsentApp.js
+++ Resources/Public/JavaScript/Src/ConsentApp.js
@@ -2,93 +2,47 @@ const ConsentApp = new function () {
     //-- global variables ---
     window.dataLayer = window.dataLayer || [];
 
-    //--- private variables ---
-    var cloneButttonInterval, counter = 0;
-
     //--- public functions ---
     /**
      * Callback function for GoogleTagManager Script to fire the dataLayer trigger
-     * @param bool state
-     * @param object app
+     * @param consent
+     * @param service
      */
-    this.consentChanged = function (state, app) {
-        if (state === true) {
-            if (app.name.indexOf('google-tagmanager-service') !== -1) {
-                let tempObj = {
-                    event: app.gtm.trigger
-                };
-                tempObj[app.gtm.variable] = true;
-                window.dataLayer.push(tempObj);
-
-                /*
-                //ES6 - https://stackoverflow.com/questions/11508463/javascript-set-object-key-by-variable
+    this.consentChanged = function (consent, service) {
+        if (consent === true) {
+            if (service.name.indexOf('google-tagmanager-service') !== -1) {
                 window.dataLayer.push({
-                    event: app.name,
-                    [app.name]: true
+                    event: service.gtm.trigger,
+                    [service.gtm.variable]: true
                 });
-                */
             }
         }
 
         //Check if the own callback function is allready defined
-        if (typeof window[app.ownCallback] === "function") {
-            window[app.ownCallback](state, app);
-        } else if (app.ownCallback !== '') {
-            console.error('The Callback function ' + app.ownCallback + ' is not yet defined. Please create it first.');
+        if (typeof window[service.ownCallback] === "function") {
+            window[service.ownCallback](consent, service);
+        } else if (service.ownCallback !== '') {
+            console.error('The Callback function ' + service.ownCallback + ' is not yet defined. Please create it first.');
         }
     };
 
     /**
-     * Replace the klaro "OK" button with a clone to remove all attached event listeners.
-     * Otherwise our own defined listener gets not fired.
-     */
-    this.cloneButton = function () {
-        if ($('.cookie-notice .cm-btn-success').length > 0) {
-            let currentButton = $('.cookie-notice .cm-btn-success');
-            let cloneButton = currentButton.clone();
-            let configLink = $('.cookie-notice p a');
-
-            //Replace the original button with the new
-            currentButton.replaceWith(cloneButton);
-
-            //Workaround to link the privacypage inside the consent text https://github.com/KIProtect/klaro/issues/116
-            $('.cn-body p:first').html(function (index, text) {
-                let link = $('<a>', {
-                    text: klaroConfig.translations.en.consentModal.privacyPolicy.name,
-                    title: klaroConfig.translations.en.consentModal.privacyPolicy.name,
-                    href: klaroConfig.privacyPolicy,
-                    class: 'privcyPage'
-                })
-                return text.replace("[privacyPage]", link[0].outerHTML);
-            });
-
-            clearInterval(cloneButttonInterval);
-            counter = 0;
-        }
-
-        if (counter > 1000) {
-            clearInterval(cloneButttonInterval);
-        }
-        counter++;
-    };
-
-    /**
-     * Evaluates the state of the consent and initiates all further opt-in modifications.
+     * Evaluates the consent and initiates all further opt-in modifications.
      *
-     * @param state
-     * @param app
+     * @param consent
+     * @param service
      * @param $element
      * @param $parentElement
      */
-    this.checkConsentOptIn = function (state, app, $element, $parentElement) {
-        if (!state) {
+    this.checkConsentOptIn = function (consent, service, $element, $parentElement) {
+        if (!consent) {
             $element.hide();
-            $parentElement.prepend(this.createConsentOptInContainer(app));
+            $parentElement.prepend(this.createConsentOptInContainer(service));
 
-            $('button[data-optin="' + app.name + '"]').click(function () {
-                klaro.getManager().updateConsent(app.name, true);
+            $('button[data-optin="' + service.name + '"]').click(function () {
+                klaro.getManager().updateConsent(service.name, true);
 
-                if ($('input[data-optin-always="' + app.name + '"]').is(':checked')) {
+                if ($('input[data-optin-always="' + service.name + '"]').is(':checked')) {
                     klaro.getManager().saveAndApplyConsents();
                 } else {
                     klaro.getManager().applyConsents();
@@ -101,48 +55,20 @@ const ConsentApp = new function () {
     };
 
     /**
-     * Creates a service opt-in container based on the provided app.
+     * Creates a service opt-in container based on the provided service.
      *
-     * @param app
+     * @param service
      * @returns {string}
      */
-    this.createConsentOptInContainer = function (app) {
-        return `<div data-service="${app.name}" class="service-opt-in">
-            <p class="lead">${app.title}</p>
-            ${app.optInText}
-            <button class="btn btn-primary btn-sm" data-optin="${app.name}">${klaroConfig.translations.en.activate}</button>
+    this.createConsentOptInContainer = function (service) {
+        return `<div data-service="${service.name}" class="service-opt-in">
+            <p class="lead">${service.title}</p>
+            ${service.optInText}
+            <button class="btn btn-primary btn-sm" data-optin="${service.name}">${klaroConfig.translations.en.activate}</button>
               <div class="form-check">
-                <input id="optin-always-${app.name}" type="checkbox" class="form-check-input" data-optin-always="${app.name}" checked>
-                <label for="optin-always-${app.name}" class="form-check-label">${klaroConfig.translations.en.activateAlways}</label>
+                <input id="optin-always-${service.name}" type="checkbox" class="form-check-input" data-optin-always="${service.name}" checked>
+                <label for="optin-always-${service.name}" class="form-check-label">${klaroConfig.translations.en.activateAlways}</label>
             </div>
           </div>`;
     };
-
-    //--- constructor ---
-    (function construct() {
-        $(document).ready(function () {
-            //Workaround if modal ist closed without save
-            $(document).on('click', 'div.cm-bg, button.hide', function (event) {
-                event.preventDefault();
-                ConsentApp.cloneButton();
-            });
-
-            //Only execute, if no decision is made yet (first visit).
-            if (klaro.getManager().confirmed === false) {
-                //Workaround for issue https://github.com/KIProtect/klaro/issues/138
-                $(document).on('click', '.cookie-notice .cm-btn-success', function (event) {
-                    klaroConfig.services.forEach(function (element, index) {
-                        klaroConfig.services[index].default = klaroConfig.services[index].defaultIfNoConsent;
-                    });
-                    klaro.getManager().resetConsents();
-                    klaro.getManager().saveAndApplyConsents();
-
-                    $('.cookie-notice ').remove();
-                });
-
-                //Workaround, because there is no klaro callback function
-                cloneButttonInterval = setInterval(ConsentApp.cloneButton, 1);
-            }
-        });
-    })();
 };
