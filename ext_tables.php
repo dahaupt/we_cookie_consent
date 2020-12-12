f082bf6692973950d766bcabec92cb8bbab94175
Daniel Haupt <dh@schiffel.it>
2020-12-12 22:09:26 +0100

    [FEATURE] Add setting to hide the backend module
    This new extension setting allows integrators to disable the backend
    module in case it is not needed at all.

--- ext_tables.php
+++ ext_tables.php
@@ -4,21 +4,26 @@ defined('TYPO3_MODE') || die('Access denied.');
 call_user_func(
     function () {
         if (TYPO3_MODE === 'BE') {
-            \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-                'Websedit.WeCookieConsent',
-                'web', // Make module a submodule of 'web'
-                'mod1', // Submodule key
-                '', // Position
-                [
-                    'Backend' => 'gtmWizard, jsonDownload',
-                ],
-                [
-                    'access' => 'user,group',
-                    'icon' => 'EXT:we_cookie_consent/Resources/Public/Icons/user_mod_mod1.svg',
-                    'labels' => 'LLL:EXT:we_cookie_consent/Resources/Private/Language/locallang_mod1.xlf',
-                ]
+            $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+                \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
             );
 
+            if ((bool)$extensionConfiguration->get('we_cookie_consent', 'showBackendModule')) {
+                \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+                    'Websedit.WeCookieConsent',
+                    'web', // Make module a submodule of 'web'
+                    'mod1', // Submodule key
+                    '', // Position
+                    [
+                        'Backend' => 'gtmWizard, jsonDownload',
+                    ],
+                    [
+                        'access' => 'user,group',
+                        'icon' => 'EXT:we_cookie_consent/Resources/Public/Icons/user_mod_mod1.svg',
+                        'labels' => 'LLL:EXT:we_cookie_consent/Resources/Private/Language/locallang_mod1.xlf',
+                    ]
+                );
+            }
         }
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_wecookieconsent_domain_model_service');
